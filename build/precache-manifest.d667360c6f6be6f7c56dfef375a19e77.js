37b2ce27f6aa52c55ac24b6c141fac7bd013f4aa
sumanmaji4 <sumanmaji736@gmail.com>
2023-09-27 20:21:48 +0530

    first commit

---
+++ build/precache-manifest.d667360c6f6be6f7c56dfef375a19e77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c8b7acd4249f3cc344c",
+    "url": "/static/css/main.904fcc21.chunk.css"
+  },
+  {
+    "revision": "5c8b7acd4249f3cc344c",
+    "url": "/static/js/main.5c8b7acd.chunk.js"
+  },
+  {
+    "revision": "7107cfed4508b2c3bdcd",
+    "url": "/static/js/1.7107cfed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3852fe7cad541d6d2ffa32df9b659ae5",
+    "url": "/index.html"
+  }
+];
